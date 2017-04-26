c0b06d44e31f754b03d1d980141168ec61551a13
Issue <zhushisong@skio.com>
2017-04-26 15:10:08 +0800

    generator

---
+++ lib/generators/detail_config.rb
@@ -0,0 +1,12 @@
+module DetailParser
+	module Generators
+		class DetailConfig < Rails::Generators::Base
+			source_root File.expand_path("./templates", __FILE__)
+
+			def copy_initializer
+        template "detail_parser.rb", "config/initializers/detail_parser.rb"
+			end
+
+		end
+	end
+end
---
+++ lib/generators/templates/detail_parser.rb
@@ -0,0 +1,3 @@
+Rails.application.configure do
+  config.detail_parser.enabled = true
+end
