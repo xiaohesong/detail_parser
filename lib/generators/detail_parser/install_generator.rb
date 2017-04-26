db588bf3a01e6f9f5e15213fe2f765f8c2128d21
Issue <zhushisong@skio.com>
2017-04-26 16:14:39 +0800

    change path

--- lib/generators/detail_parser/install_generator.rb
+++ lib/generators/detail_parser/install_generator.rb
@@ -1,7 +1,7 @@
 module DetailParser
 	module Generators
 		class InstallGenerator < Rails::Generators::Base
-			source_root File.expand_path("../templates", __FILE__)
+			source_root File.expand_path("../../templates", __FILE__)
 
 			def copy_initializer
         template "detail_parser.rb", "config/initializers/detail_parser.rb"
