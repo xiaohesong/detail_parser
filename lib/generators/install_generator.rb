70bada523055c20ff769270cb4017059b4d5cff6
Issue <zhushisong@skio.com>
2017-04-26 15:44:26 +0800

    rename

--- lib/generators/install_generator.rb
+++ lib/generators/install_generator.rb
@@ -1,6 +1,6 @@
 module DetailParser
 	module Generators
-		class DetailConfig < Rails::Generators::Base
+		class InstallGenerator < Rails::Generators::Base
 			source_root File.expand_path("./templates", __FILE__)
 
 			def copy_initializer
