bc7fd102f84be5e5c30e5d7f5b37fd7c915911ef
Issue <zhushisong@skio.com>
2017-05-02 16:26:14 +0800

    log puts

--- lib/detail_parser/log_subscriber.rb
+++ lib/detail_parser/log_subscriber.rb
@@ -9,6 +9,7 @@ module DetailParser
   class LogSubscriber < ActiveSupport::LogSubscriber
 
     def process_action(event)
+      puts "When the log subscriber is used with process_action"
       payload = event.payload
       data = extract_request(event, payload)
       data = before_format(data, payload)
