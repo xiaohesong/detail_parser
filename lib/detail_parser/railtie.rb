ad379c2adafdc197e249afe2f949e856b5089914
Issue <zhushisong@skio.com>
2017-05-03 16:46:07 +0800

    fix

--- lib/detail_parser/railtie.rb
+++ lib/detail_parser/railtie.rb
@@ -5,6 +5,10 @@ require 'action_controller/log_subscriber'
 class DetailParser::Railtie < Rails::Railtie
   env = Rails.env.to_sym || :development
 
+
+  config.detail_parser = ActiveSupport::OrderedOptions.new
+  config.detail_parser.enabled = false
+
   initializer 'detail parser logger', after: :load_config_initializers do
     DetailParser.logger = Rails.application.config.logger || Rails.logger.presence
     DetailParser.filter = ActionDispatch::Http::ParameterFilter.new Rails.application.config.filter_parameters
