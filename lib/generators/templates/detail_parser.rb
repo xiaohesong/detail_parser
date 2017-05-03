daee389a1b4483842967f418d38e8a72418069f5
Issue <zhushisong@skio.com>
2017-05-03 13:30:26 +0800

    add apend to

--- lib/generators/templates/detail_parser.rb
+++ lib/generators/templates/detail_parser.rb
@@ -1,3 +1,29 @@
 Rails.application.configure do
   config.detail_parser.enabled = true
 end
+
+
+ActionController::Instrumentation.class_eval do
+  def process_action(*args)
+    raw_payload = {
+      controller: self.class.name,
+      action:     self.action_name,
+      params:     request.filtered_parameters,
+      format:     request.format.try(:ref),
+      method:     request.method,
+      path:       (request.fullpath rescue "unknown"),
+      request:    request,
+      response:   response,
+      session:    session
+    }
+
+    ActiveSupport::Notifications.instrument("start_processing.action_controller", raw_payload.dup)
+
+    ActiveSupport::Notifications.instrument("process_action.action_controller", raw_payload) do |payload|
+      result = super
+      payload[:status] = response.status
+      append_info_to_payload(payload)
+      result
+    end
+  end
+end
